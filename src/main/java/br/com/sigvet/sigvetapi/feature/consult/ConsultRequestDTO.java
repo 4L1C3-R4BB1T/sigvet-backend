c0c96a7528f226e672d663a8353bda593f18c78d
Livia <livinha.guimaraes.013@gmail.com>
2024-04-22 16:02:01 -0300

    feat: Validação nos DTOs

--- src/main/java/br/com/sigvet/sigvetapi/feature/consult/ConsultRequestDTO.java
+++ src/main/java/br/com/sigvet/sigvetapi/feature/consult/ConsultRequestDTO.java
@@ -3,13 +3,22 @@ package br.com.sigvet.sigvetapi.feature.consult;
 import java.time.LocalDateTime;
 
 import br.com.sigvet.sigvetapi.common.entities.enums.ConsultationStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 
 public record ConsultRequestDTO(
-    LocalDateTime dateTime,
-    Long veterinarianId,
-    Long animalId,
-    ConsultationStatus status
-) {
 
-   
-}
+    @NotNull(message = "Date Time can't be null") 
+    @FutureOrPresent(message = "Date Time should be future or present")
+    LocalDateTime dateTime,
+
+    @NotNull(message = "Veterinarian ID can't be null") 
+    Long veterinarianId,
+
+    @NotNull(message = "Animal ID can't be null") 
+    Long animalId,
+
+    @NotNull(message = "Status can't be null") 
+    ConsultationStatus status
+
+) {}
