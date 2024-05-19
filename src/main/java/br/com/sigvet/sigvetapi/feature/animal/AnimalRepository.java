2e4dee579a06ba87a89cd023e9b604081a4b8948
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-05-19 08:56:02 -0300

    feat: Novos endpoints para client e animais

--- src/main/java/br/com/sigvet/sigvetapi/feature/animal/AnimalRepository.java
+++ src/main/java/br/com/sigvet/sigvetapi/feature/animal/AnimalRepository.java
@@ -10,8 +10,12 @@ import br.com.sigvet.sigvetapi.common.entities.AnimalEntity;
 
 public interface AnimalRepository extends JpaRepository<AnimalEntity, Long>, JpaSpecificationExecutor<AnimalEntity> {
 
-    @Query(value = "SELECT * FROM animals a WHERE LOWER(unaccent(a.name)) LIKE unaccent(CONCAT('%', LOWER(?1), '%')) AND u.deleted = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM animals a WHERE LOWER(unaccent(a.name)) LIKE unaccent(CONCAT('%', LOWER(?1), '%')) AND u.deleted is false", nativeQuery = true)
     List<AnimalEntity> searchByName(String name);
 
-    List<AnimalEntity> findByClientId(Long id);
+    List<AnimalEntity> findByClientIdAndDeleted(Long id, boolean deleted);
+
+    default List<AnimalEntity> findByClientId(Long id) {
+        return findByClientIdAndDeleted(id, false);
+    }
 }
