a9e9a4ce2ad4f3542a190b4f29c07752034da76e
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-05-25 11:41:26 -0300

    refactor: Refatorando lógica para vacinação

--- src/main/java/br/com/sigvet/sigvetapi/feature/photo/PhotoRequestDTO.java
+++ src/main/java/br/com/sigvet/sigvetapi/feature/photo/PhotoRequestDTO.java
@@ -11,7 +11,7 @@ import jakarta.validation.constraints.NotNull;
 @Tag(name = "Foto")
 public record PhotoRequestDTO(
     @NotNull(message = "The Upload cannot be null")
-    @FileContentType(allowed = { MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE }, message = "Only PNG and JPEG is allowed.")
+    @FileContentType(allowed = { MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE }, message = "Apenas PNG e JPEG são permitidos")
     @FileSize(max = "5MB")
     MultipartFile photo
 ) {}
